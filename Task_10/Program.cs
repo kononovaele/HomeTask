0583feda71545bef06c96662827a964f1113d59a
Kononovaele <Kononovaele@yandex.ru>
2023-01-08 00:35:47 +0300

    start task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10. Программа принимает на взходе 3-х значеное число
+// и на выходе показывает 2-ую цифрcdу этого числа
+Console.WriteLine(" --- Task 10 ---");
+
+Console.Write("Input three-digit number, please: ");
+String strDigin = Console.ReadLine();
+Console.Write("The second digit is: ");
+Console.WriteLine(strDigin[1]);
