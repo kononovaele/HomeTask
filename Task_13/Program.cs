dde3472ee34b738ea19e3a7cc6b0677a3b153ca9
Kononovaele <Kononovaele@yandex.ru>
2023-01-08 00:54:46 +0300

    add all files in tasks

---
+++ Task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 15. Программа выводит 3-ую цифру введенного числа
+// если третьей цифры нет, то ругается
+
+Console.WriteLine(" --- Task 15 ---");
+
+Console.Write("Input any digit, please: ");
+string strDigit = Console.ReadLine();
+if( strDigit.Length < 3 ) {
+    Console.WriteLine("There is not third digit, sorry!!!");    
+}
+else {
+    Console.Write("The third digit is: ");
+    Console.WriteLine(strDigit[2]);
+}
---
+++ Task_15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 15. Программа получает единственное число, день недели (1, 2 ... 7) и на
+// выходе печатает Yes - если это выходной и No - если тяжелые будни
+
+Console.WriteLine(" --- Task 15 ---");
+
+Console.Write("Input digit, day of week (1, 2 ... 7), please: ");
+string strDigit = Console.ReadLine();
+if( strDigit == "6" || strDigit == "7" ) {
+    Console.WriteLine("Yes");
+}
+else {
+    Console.Write("No");
+}
