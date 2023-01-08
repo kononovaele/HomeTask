69c6c7145241bd96da5dc6d8bb73b1a72c86e3f6
Kononovaele <Kononovaele@yandex.ru>
2023-01-08 14:55:50 +0300

    add aditional file, draw.io and others

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Задача 10. Программа принимает на взходе 3-х значеное число
-// и на выходе показывает 2-ую цифрcdу этого числа
+// и на выходе показывает 2-ую цифру этого числа
 Console.WriteLine(" --- Task 10 ---");
 
-Console.Write("Input three-digit number, please: ");
+Console.Write("Input three-digit digit, please: ");
 String strDigin = Console.ReadLine();
 Console.Write("The second digit is: ");
 Console.WriteLine(strDigin[1]);
